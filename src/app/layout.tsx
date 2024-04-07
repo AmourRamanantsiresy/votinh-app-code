47c9617b0b2e728f167e70152498a3e5f27298af
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-04-07 11:57:20 +0300

    chore: update app title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'My portfolio',
+  title: 'Voting App',
+  description: 'Voting App',
 };
 
 export default function RootLayout({
